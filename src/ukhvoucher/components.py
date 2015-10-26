f96ba4da5c52406b55b2b3ecfc3a0f697b7625d6
Christian Klinger <ck@novareto.de>
2015-10-26 11:55:11 +0100

    add components

---
+++ src/ukhvoucher/components.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2007-2013 NovaReto GmbH
+# cklinger@novareto.de 
+
+from ul.auth import Principal
+
+
+class ExternalPrincipal(Principal):
+
+    def getAddress(self):
+        pass
+
+    def getCategory(self):
+        pass
