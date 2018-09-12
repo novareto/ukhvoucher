971b2c184c4781c769d4f1882eba6a557de85c58
Christian Klinger <ck@novareto.de>
2018-09-12 09:35:59 +0200

    start time range

--- src/ukhvoucher/browser/masquarade.py
+++ src/ukhvoucher/browser/masquarade.py
@@ -7,11 +7,15 @@ from cromlech.browser import getSession
 from zope.schema import Choice
 from zope.interface import Interface
 from ukhvoucher.browser.views import AdminRootIndex
+from ukhvoucher.vocabularies import VOCABULARIES
 from ..interfaces import IAdminLayer, get_oid
 from profilehooks import profile
 from plone.memoize import forever
 from plone.memoize import ram
 from time import time
+from uvc.entities.browser.menus import IPersonalMenu
+from ukhtheme.uvclight.viewlets import Navigation
+from dolmen.forms.base.datamanagers import DictDataManager
 
 
 class IAccounts(Interface):
@@ -58,3 +62,50 @@ class UserMasquarade(uvclight.ViewletForm):
         form = uvclight.ViewletForm.render(self)
         return form
 
+
+class IDateRange(Interface):
+    """ Date Range Select"""
+
+    date_range = Choice(
+        title=u"Abrechungszeitraun auswählen",
+        source = VOCABULARIES['abrechnungszeitraum'](None) 
+        )
+
+
+class TimeRanngeSelect(uvclight.ViewletForm):
+    uvclight.viewletmanager(Navigation)
+    uvclight.layer(IAdminLayer)
+    uvclight.auth.require('display.vouchers')
+    uvclight.context(Interface)
+    uvclight.order(59)
+    template = uvclight.get_template('timerangeselect.cpt', __file__)
+    fields = uvclight.Fields(IDateRange)
+    ignoreContent = False
+    #dataManager = DictDataManager
+
+    def __init__1(self, *args):
+        uvclight.ViewletForm.__init__(self, *args)
+        session = getSession()
+        print session
+        self.setContentData(session)
+
+    @property
+    def action_url(self):
+        return self.view.application_url()
+
+    @uvclight.action(u'Abrechnungszeitraum wählen', identifier="cdr")
+    def handle_date_range(self):
+        data, errors = self.extractData()
+        if errors:
+            return
+        import pdb; pdb.set_trace()
+        session = getSession()
+
+        print session
+        session['date_range'] = data['date_range']
+        self.view.flash(u'Der Abrechungszeitraum wurde gesetzt!')
+        self.view.redirect(self.request.path)
+
+    def render(self):
+        form = uvclight.ViewletForm.render(self)
+        return form
